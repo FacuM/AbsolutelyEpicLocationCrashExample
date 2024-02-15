9bb4b9efb4c8352a56c8cee07c545a695cc1dcd6
FacuM <facumo.fm@gmail.com>
2024-02-15 01:38:57 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [ message, setMessage ] = useState();
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+  
+    console.debug('requestForegroundPermissionsAsync:', status);
+  
+    if (status !== 'granted') {
+      setMessage('status: ' + status);
+  
+      return;
+    }
+  
+    let location;
+
+    try {
+      location = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.Low
+      });
+
+      console.debug('location:', location);
+
+      setMessage(
+        JSON.stringify(location)
+      );
+    } catch (exception) {
+      console.error(exception);
+
+      setMessage(exception.message);
+
+      return;
+    }
+  };
+
+  useState(() => { getLocation(); }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{
+        color: 'black',
+        textAlign: 'center'
+      }}> {message} </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
